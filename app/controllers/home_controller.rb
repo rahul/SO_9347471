c8fcb8fe2f137e8b137cc2c945842441480fe69e
Rahul Sarika <rahul.sarika@gmail.com>
2012-02-22 12:11:49 +0530

    tries to mimick OP's situation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-
+    @cars = Car.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times {Car.create(:description => Faker::Lorem.paragraph)}
